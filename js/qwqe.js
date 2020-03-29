2c095a916b4daaac3175cbd5f95e69efefdfbca9
ivan300petrov <38078954+ivan300petrov@users.noreply.github.com>
2020-03-29 20:33:15 +0700

    кукпув

--- js/qwqe.js
+++ js/qwqe.js
@@ -1,15 +1,15 @@
 var galleryItemsArray = [];
-var galleryItems = document.querySelectorAll(".gallery-link");
+var galleryItems = document.querySelectorAll(".gallery-item");
 
 for (var i = 0; i < galleryItems.length; i++) {
 	galleryItemsArray[i] = galleryItems[i].innerHTML;
 	galleryItems[i].addEventListener("click", function showPhoto(e) {
 		e.preventDefault();
-		$(".regular").slick({
-			dots: true,
-			infinite: true,
-			slidesToShow: 1,
-			slidesToScroll: 1
+		$(".variable").slick({
+			dots: false
+			//infinite: true,
+			//	slidesToShow: 1,
+			//slidesToScroll: 1
 		});
 		//  galleryPopup.style.display = 'flex';
 		//galleryPopupImage.src = this.href;
