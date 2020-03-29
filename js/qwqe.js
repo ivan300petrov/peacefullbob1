b84d2ee1958e233937f4731735ddf1fbb9f2b2d0
ivan300petrov <38078954+ivan300petrov@users.noreply.github.com>
2020-03-29 14:04:03 +0700

    фаыф

--- js/qwqe.js
+++ js/qwqe.js
@@ -1,1 +1,19 @@
+var galleryItemsArray = [];
+var galleryItems = document.querySelectorAll(".gallery-link");
+
+for (var i = 0; i < galleryItems.length; i++) {
+	galleryItemsArray[i] = galleryItems[i].innerHTML;
+	galleryItems[i].addEventListener("click", function showPhoto(e) {
+		e.preventDefault();
+		$(".regular").slick({
+			dots: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1
+		});
+		//  galleryPopup.style.display = 'flex';
+		//galleryPopupImage.src = this.href;
+	});
+}
+
 $(".single-item").slick();
